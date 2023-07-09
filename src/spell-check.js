73976d290f0d9c14bc74c29f78b736c0d536c801
christian-post <chr.post@gmx.net>
2023-07-09 17:45:00 +0200

    fixed common words file read

--- src/spell-check.js
+++ src/spell-check.js
@@ -3,7 +3,14 @@ const utils = require('./utils');
 
 const FPATH = './data/rotation_rumble_common_words.txt';
 const commonWordsRaw = fs.readFileSync(FPATH, { encoding: 'utf8', flag: 'r' });
-const commonWords = commonWordsRaw.split('\r\n');
+
+let commonWords;
+if (commonWordsRaw.includes('\r')) {
+  commonWords = commonWordsRaw.split('\r\n');
+} else {
+  commonWords = commonWordsRaw.split('\n');
+}
+
 
 
 module.exports = {
